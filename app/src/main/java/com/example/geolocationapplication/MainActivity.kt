6910d632c215283f278fcd0c31f38630711b9759
marsel <dilyara-gabdulkhakova@mail.ru>
2021-05-28 13:35:33 +0300

    Создание сервиса, разрешение на геолокацию и запуск сервиса

--- app/src/main/java/com/example/geolocationapplication/MainActivity.kt
+++ app/src/main/java/com/example/geolocationapplication/MainActivity.kt
@@ -1,7 +1,10 @@
 package com.example.geolocationapplication
 
-import androidx.appcompat.app.AppCompatActivity
+import android.Manifest
+import android.content.Intent
+import android.content.pm.PackageManager
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -10,10 +13,41 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         btn_start.setOnClickListener {
-            tv_service_state.text = "Сервис включен"
+            if (this.checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED)
+                requestLocationPermission()
+            else {
+                tv_service_state.text = "Сервис включен"
+                sendServiceCommand("start_service")
+            }
         }
         btn_stop.setOnClickListener {
             tv_service_state.text = "Сервис выключен"
+            sendServiceCommand("stop_service")
         }
     }
+
+    private fun sendServiceCommand(command: String) {
+        val intent = Intent(this, LocationService::class.java)
+        intent.putExtra("command", command)
+        startService(intent)
+    }
+
+    private fun requestLocationPermission() {
+        this.requestPermissions(arrayOf(Manifest.permission.ACCESS_FINE_LOCATION), 55)
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == 55
+            && permissions.first() == Manifest.permission.ACCESS_FINE_LOCATION
+            && grantResults.first() == PackageManager.PERMISSION_GRANTED
+        ) {
+            tv_service_state.text = "Сервис включен"
+        }
+    }
+
 }
