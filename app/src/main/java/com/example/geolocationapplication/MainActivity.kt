a3926ce8a9987f6ff32ad023ffce726f6614113e
marsel <dilyara-gabdulkhakova@mail.ru>
2021-05-28 13:08:46 +0300

    Дизайн приложения

--- app/src/main/java/com/example/geolocationapplication/MainActivity.kt
+++ app/src/main/java/com/example/geolocationapplication/MainActivity.kt
@@ -2,10 +2,18 @@ package com.example.geolocationapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btn_start.setOnClickListener {
+            tv_service_state.text = "Сервис включен"
+        }
+        btn_stop.setOnClickListener {
+            tv_service_state.text = "Сервис выключен"
+        }
     }
 }
